6675cd63165b8f6e1fe1deffb7989f0e2723b646
Farruco Sanjurjo <madtrick@aire.local>
2014-02-15 19:03:24 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amora::Application.config.secret_key_base = '044275653d5ade27a08a9f4bb317e78338670661ed7efaed05fb83c75ff5baf47e1d5c7306d46c3f95e278e7db945a0d1ac23c0db2fc520fe4f84c73bc3f7902'
